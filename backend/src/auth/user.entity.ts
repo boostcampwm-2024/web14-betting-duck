34c972883d589dfe0f9d42e19ad72cd571af65a6
정동교 <jdk8467@naver.com>
2024-11-09 23:39:49 +0900

    feat: entity 생성 [#91]

---
+++ backend/src/auth/user.entity.ts
@@ -0,0 +1,39 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  OneToMany,
+  CreateDateColumn,
+} from "typeorm";
+import { Bet } from "src/bet/bet.entity";
+import { BetRoom } from "src/bet-room/bet-room.entity";
+
+@Entity()
+@Unique(["nickname"])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  duck: number;
+
+  @CreateDateColumn({ type: "timestamp", nullable: true })
+  created_at: Date;
+
+  @OneToMany(() => Bet, (bet) => bet.user)
+  bets: Bet[];
+
+  @OneToMany(() => BetRoom, (betRoom) => betRoom.manager)
+  managedBetRooms: BetRoom[];
+}
---
+++ backend/src/bet-room/bet-room.entity.ts
@@ -0,0 +1,61 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "src/auth/user.entity";
+import { Bet } from "../bet/bet.entity";
+import { BetResult } from "../bet-room/bet-result.entity";
+
+@Entity("bet_rooms")
+export class BetRoom extends BaseEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.managedBetRooms)
+  manager: User;
+
+  @Column({ length: 300 })
+  title: string;
+
+  @Column()
+  defaultBetAmount: number;
+
+  @Column({ length: 200 })
+  option1: string;
+
+  @Column({ length: 200 })
+  option2: string;
+
+  @Column({ type: "timestamp", nullable: true })
+  startTime: Date;
+
+  @Column({ type: "timestamp", nullable: true })
+  endTime: Date;
+
+  @Column({ type: "enum", enum: ["open", "closed", "pending"] })
+  status: "open" | "closed" | "pending";
+
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp", nullable: true })
+  updatedAt: Date;
+
+  @Column({ length: 200 })
+  joinUrl: string;
+
+  @Column({ type: "timestamp", nullable: true })
+  timer: Date;
+
+  @OneToMany(() => Bet, (bet) => bet.betRoom)
+  bets: Bet[];
+
+  @OneToMany(() => BetResult, (betResult) => betResult.betRoom)
+  betResults: BetResult[];
+}
