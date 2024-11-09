ac20e2ba084c664d57b6634118db45c99fab1d67
정동교 <jdk8467@naver.com>
2024-11-10 00:12:15 +0900

    chore: nestjs/config 설치 및 환경 변수 configservice로 관리 [#91]

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,12 +1,17 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: "postgres",
-  host: process.env.AUTH_POSTGRES_HOSTNAME,
-  port: parseInt(process.env.AUTH_DATABASE_PORT, 10),
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.AUTH_POSTGRES_DB_NAME,
-  entities: [__dirname + "/../**/*.entity.{js,ts}"],
-  synchronize: true,
+export const typeORMConfig = async (
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> => {
+  return {
+    type: "postgres",
+    host: configService.get<string>("POSTGRES_HOSTNAME"),
+    port: configService.get<number>("POSTGRES_PORT"),
+    username: configService.get<string>("POSTGRES_USERNAME"),
+    password: configService.get<string>("POSTGRES_PASSWORD"),
+    database: configService.get<string>("POSTGRES_DB_NAME"),
+    entities: [__dirname + "/../**/*.entity.{js,ts}"],
+    synchronize: configService.get<boolean>("POSTGRES_DB_NAME_DB_SYNCHRONIZE"),
+  };
 };
