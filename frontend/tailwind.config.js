dff32ee96c1ed32615403f0bd643f27352750283
sunub <bsc5672@gmail.com>
2024-11-12 11:29:13 +0900

    feat : 특정 스타일 규칙들 추가 [#95]

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,35 @@
+import plugin from "tailwindcss/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        layout: "auto 1fr",
+      },
+      fontFamily: {
+        "nanum-eb": ["ExtraBold", "sans-serif"],
+        "nanum-b": ["Bold", "sans-serif"],
+        "nanum-r": ["Regular", "sans-serif"],
+      },
+      backgroundColor: {
+        blur: "color-mix(in oklch, oklch(37.92% 0.039 257.29) 30%,transparent)",
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".fixed-center": {
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "fit-content",
+          zIndex: "1000",
+        },
+      });
+    }),
+  ],
 };
