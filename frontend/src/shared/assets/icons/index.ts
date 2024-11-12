74d7a8959f4de6ea9a6c3053bc5760557cb05867
sunub <bsc5672@gmail.com>
2024-11-12 11:22:52 +0900

    feat : 레이아웃에서 사용하는 아이콘을 별도의 컴포넌트로 생성 [#95]

---
+++ frontend/src/shared/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import { LoginIcon } from "./LoginIcon";
+import { LogoIcon } from "./LogoIcon";
+import { UserIcon } from "./UserIcon";
+import { ChatIcon } from "./ChatIcon";
+import { CreateVoteIcon } from "./CreateVoteIcon";
+
+export { LoginIcon, LogoIcon, UserIcon, ChatIcon, CreateVoteIcon };
