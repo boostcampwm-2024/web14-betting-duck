49dd2d8ab734ded34f74d387787febfe8f7b74cb
정동교 <jdk8467@naver.com>
2024-11-14 18:54:47 +0900

    refactor: option enum 스트링 수정 [#29]

--- shared/schemas/bet/socket/request.ts
+++ shared/schemas/bet/socket/request.ts
@@ -9,7 +9,7 @@ export const joinBetRoomRequestSchema = z.object({
     sender: z.object({
         nickname: z.string().min(1, '닉네임이 필요합니다.'),
         betAmount: z.number().min(0, '베팅 금액은 0 이상이어야 합니다.'),
-        selectOption: z.enum(['1', '2'], { message: '선택 옵션은 1 또는 2이어야 합니다.' }),
+        selectOption: z.enum(['option1', 'option2'], { message: '선택 옵션은 1 또는 2이어야 합니다.' }),
     }),
     channel: roomIdSchema,
 });
