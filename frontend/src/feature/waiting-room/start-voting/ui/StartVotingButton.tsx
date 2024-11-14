2a126bdf71af0712f7a0930abd80987aa0e52c74
sunub <bsc5672@gmail.com>
2024-11-14 18:46:43 +0900

    refactor : 사용하지 않는 컴포넌트 제거 [#117]

--- frontend/src/feature/waiting-room/start-voting/ui/StartVotingButton.tsx
+++
@@ -1,9 +0,0 @@
-function StartVotingButton() {
-  return (
-    <button className="bg-default-default text-secondary-default w-full rounded-lg p-[10px]">
-      투표 시작
-    </button>
-  );
-}
-
-export { StartVotingButton };
