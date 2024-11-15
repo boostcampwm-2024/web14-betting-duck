54152cb799ec5f0e06554ecb0b6a0b67b408dab1
sunub <bsc5672@gmail.com>
2024-11-15 18:52:20 +0900

    feat : action을 작동시키기 위한 트리거 메세지

--- frontend/src/pages/WaitingRoom/WaitingRoomHeader.tsx
+++ frontend/src/pages/WaitingRoom/WaitingRoomHeader.tsx
@@ -1,7 +1,7 @@
 function WaitingRoomHeader() {
   return (
     <div className="flex items-center justify-center p-6">
-      <h1 className="font-nanum-eb text-4xl">투표 대기창</h1>
+      <h1 className="font-nanum-eb text-4xl">투표대기창</h1>
     </div>
   );
 }
