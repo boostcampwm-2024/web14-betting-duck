a57703e1397d53ac078675644e795117826fcb02
sunub <bsc5672@gmail.com>
2024-11-15 18:25:20 +0900

    refator : 대기방에 대한 구조 개선 중 [#117]

--- frontend/src/features/waiting-room/start-voting/ui/StartVotingButton.tsx
+++ frontend/src/features/waiting-room/start-voting/ui/StartVotingButton.tsx
@@ -1,6 +1,6 @@
 function StartVotingButton() {
   return (
-    <button className="bg-default-default text-secondary-default w-full rounded-lg p-[10px]">
+    <button className="bg-default text-secondary w-full rounded-lg p-[10px]">
       투표 시작
     </button>
   );
