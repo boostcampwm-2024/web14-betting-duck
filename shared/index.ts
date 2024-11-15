47ae670cd406c044aab5a202f1134b225f2c8fef
sunub <bsc5672@gmail.com>
2024-11-15 18:30:59 +0900

    feat : socketio client 추가 [#117]

--- shared/index.ts
+++ shared/index.ts
@@ -3,3 +3,5 @@ export * from "./schemas/bettingRooms";
 export * from "./schemas/betresult";
 export * from "./schemas/users";
 export * from "./vars";
+export * from "./schemas/chat/socket/request";
+export * from "./schemas/chat/socket/reponse";
