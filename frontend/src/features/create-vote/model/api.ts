41cadc7d09bd24193347ddd7628aba88959ea126
죠 <stop.ho@khu.ac.kr>
2024-11-19 11:02:24 +0900

    feature: API 연동 [#119]

--- frontend/src/features/create-vote/model/api.ts
+++ frontend/src/features/create-vote/model/api.ts
@@ -1,6 +1,17 @@
-async function createPrediction(formData: FormData) {
-  console.log("API 요청");
-  console.log("data:", formData);
+import { PredictionRequest } from "./types";
+
+async function createPrediction(data: PredictionRequest) {
+  const response = await fetch("/api/betrooms", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error("API 요청 실패");
+  }
+
+  return response.json();
 }
 
 export { createPrediction };
