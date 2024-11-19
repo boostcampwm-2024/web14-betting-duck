1b1cfa2ca4da940d976b30f41fb19153f60780df
정동교 <jdk8467@naver.com>
2024-11-19 23:02:16 +0900

    feat: 베팅방 ID로 베팅 결과 조회 API 구현 [#56]

--- backend/src/bet-result/bet-result.module.ts
+++ backend/src/bet-result/bet-result.module.ts
@@ -2,9 +2,12 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BetResultRepository } from "src/bet-result/bet-result.repository";
 import { BetResult } from "./bet-result.entity";
+import { BetResultController } from "./bet-result.controller";
+import { BetResultService } from "./bet-result.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([BetResult])],
-  providers: [BetResultRepository],
+  controllers: [BetResultController],
+  providers: [BetResultService, BetResultRepository],
 })
 export class BetResultModule {}
