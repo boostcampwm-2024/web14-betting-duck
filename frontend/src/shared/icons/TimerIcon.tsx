8e5bfe3c23f813129700cf3e350300a993f56964
sunub <bsc5672@gmail.com>
2024-11-19 09:47:13 +0900

    refactor : props를 이용하여 스타일 작성이 가능하게 수정 [#9]

--- frontend/src/shared/icons/TimerIcon.tsx
+++ frontend/src/shared/icons/TimerIcon.tsx
@@ -1,4 +1,4 @@
-function TimerIcon() {
+function TimerIcon({ ...props }: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       width="10"
@@ -6,6 +6,7 @@ function TimerIcon() {
       viewBox="0 0 10 10"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path
         d="M5 2.6V5L6.6 5.8M9 5C9 7.20914 7.20914 9 5 9C2.79086 9 1 7.20914 1 5C1 2.79086 2.79086 1 5 1C7.20914 1 9 2.79086 9 5Z"
