547eed3e7010d15a98928765c44910f85b7f330c
LLagoon3 <choonarm3@gmail.com>
2024-11-19 15:33:42 +0900

    feat: 미들웨어 로거 추가 [#32]

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { swaggerConfig } from "./config/swagger.config";
-import { GlobalHttpExceptionFilter } from "./utils/filters/global-http-exception-filter";
+import { GlobalHttpExceptionFilter } from "./utils/filters/global-http-exception.filter";
+import { LoggerMiddleware } from "./utils/middlewares/logger.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   swaggerConfig(app);
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
+  app.use(new LoggerMiddleware().use);
   await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
 }
 bootstrap();
