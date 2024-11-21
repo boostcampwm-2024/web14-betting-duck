e20134376304719b36f39c1fda0b8d9fd5d43ea5
sunub <bsc5672@gmail.com>
2024-11-21 23:59:59 +0900

    feat : 투표 생성 후 리다이렉트 될 수 있게끔 로직 추가

--- frontend/src/features/create-vote/model/store.ts
+++ frontend/src/features/create-vote/model/store.ts
@@ -1,7 +1,11 @@
+import { useUser } from "@/shared/hooks/use-user";
 import { createPrediction } from "./api";
 import { formatPredictionData } from "./helpers/formatData";
+import { useNavigate } from "@tanstack/react-router";
 
 function usePredictionStore() {
+  const { setUserInfo, userInfo } = useUser();
+  const navigate = useNavigate();
   const submitPrediction = async (formData: FormData) => {
     const requestData = formatPredictionData({
       title: (formData.get("title") as string) || "",
@@ -12,7 +16,12 @@ function usePredictionStore() {
     });
     try {
       const result = await createPrediction(requestData);
-      console.log(result);
+      if (!result) throw new Error("실패");
+
+      setUserInfo({ ...userInfo, roomId: result.data.roomId, role: "admin" });
+      navigate({
+        to: `/voting/${result.data.roomId}/waiting`,
+      });
     } catch (error) {
       console.error("승부예측 정보 전송 오류:", error);
     }
