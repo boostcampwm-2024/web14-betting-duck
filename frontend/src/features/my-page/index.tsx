9c1347ed7cf19922c49af69df9fe1b8d554a5f38
sunub <bsc5672@gmail.com>
2024-11-21 13:32:19 +0900

    feat : MyPage Error Component 추가 [#67]

---
+++ frontend/src/features/my-page/index.tsx
@@ -0,0 +1,35 @@
+import { Image } from "@/shared/components/Image";
+import pondImage from "@/assets/images/pond.png";
+import { DuckCoinIcon } from "@/shared/icons";
+import { useUser } from "@/shared/hooks/use-user";
+
+function MyPage() {
+  const { coin } = useUser();
+
+  return (
+    <div className="w-ful bg-layout-main flex flex-col items-center justify-center gap-16">
+      <div className="flex flex-col items-center justify-center pb-6">
+        <h1 className="text-2xl font-extrabold">마이 페이지</h1>
+        <p className="text-lg">오리를 구매해서 페이지를 꾸며보세요</p>
+      </div>
+      <div className="flex w-full items-center justify-center gap-4">
+        <DuckCoinIcon width={32} height={32} />
+        <span className="text-2xl font-bold">{coin}</span>
+      </div>
+      <div className="w-full px-5">
+        <Image src={pondImage} alt="Pond" />
+      </div>
+      <div className="flex gap-8">
+        <button className="bg-secondary text-default border-default-hover flex items-center gap-4 rounded-xl border-2 px-6 py-3 text-xl font-bold">
+          <DuckCoinIcon width={32} height={33} />
+          <span>-30</span>
+        </button>
+        <button className="bg-default text-layout-main rounded-xl px-6 py-3 text-xl">
+          방 만들러 가기
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export { MyPage };
