ed221f99f6a410af7dba30b9ce24d26f0959fd63
sunub <bsc5672@gmail.com>
2024-11-21 23:53:24 +0900

    feat : 라우트 패스 추가 및 불필요한 파일 삭제

--- frontend/src/routes/waiting-room.tsx
+++
@@ -1,6 +0,0 @@
-import { WaitingRoom } from "@/features/waiting-room";
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/waiting-room")({
-  component: WaitingRoom,
-});
