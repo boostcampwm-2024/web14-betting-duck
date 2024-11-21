3aba35e545bc408990a9034136fa7c777c86b9c4
sunub <bsc5672@gmail.com>
2024-11-21 23:44:56 +0900

    feat : 소켓와 fetch를 이용하여 데이터를 불러오고 업데이트 할 수 있게 끔 코드 작성

--- frontend/src/features/waiting-room/index.tsx
+++ frontend/src/features/waiting-room/index.tsx
@@ -2,25 +2,23 @@ import { WaitingRoomHeader } from "./ui/WaitingRoomHeader";
 import { ParticipantsList } from "./ui/ParticipantsList";
 import { ShareLinkCard } from "@/features/waiting-room/ui/SharedLinkCard";
 import { AdminFooter } from "./ui/AdminFooter";
-import React from "react";
-// import { MemberFooter } from "./ui/MemberFooter";
+import { MemberFooter } from "./ui/MemberFooter";
+import { useLoaderData } from "@tanstack/react-router";
+import { useUser } from "@/shared/hooks/use-user";
 
 function WaitingRoom() {
-  React.useEffect(() => {
-    fetch("/api/users/token")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+  const { userInfo } = useUser();
+  const { roomId } = useLoaderData({ from: "/voting_/$roomId/waiting" });
 
   return (
     <div className="bg-layout-main flex h-full w-full flex-col justify-between pb-16 pt-8">
-      <WaitingRoomHeader />
+      <WaitingRoomHeader roomId={roomId} />
       <section className="flex flex-col gap-6 px-4">
-        <ParticipantsList />
+        <ParticipantsList roomId={roomId} />
         <ShareLinkCard />
       </section>
       <div className="flex flex-row gap-4 px-4 text-lg font-extrabold">
-        <AdminFooter />
+        {userInfo.role === "admin" ? <AdminFooter /> : <MemberFooter />}
       </div>
     </div>
   );
--- frontend/src/features/waiting-room/ui/WaitingRoomHeader/index.tsx
+++ frontend/src/features/waiting-room/ui/WaitingRoomHeader/index.tsx
@@ -1,12 +1,32 @@
 import { VotingStatusCard } from "./VotingStatusCard";
+import React from "react";
+import { responseBetRoomInfo } from "@betting-duck/shared";
+import { z } from "zod";
 
-function WaitingRoomHeader() {
+type WaitingRoomInfo = z.infer<typeof responseBetRoomInfo>;
+
+function WaitingRoomHeader({ roomId }: { roomId: string }) {
+  const [channelInfo, setChannelInfo] = React.useState<WaitingRoomInfo>();
+
+  React.useEffect(() => {
+    fetch(`/api/betrooms/${roomId}`)
+      .then((res) => res.json())
+      .then(({ data }) => {
+        const result = responseBetRoomInfo.safeParse(data);
+        if (!result.success) {
+          console.error(result.error.errors);
+          return;
+        }
+        setChannelInfo(result.data);
+      });
+  }, [roomId]);
+
   return (
     <div className="flex flex-col items-center justify-center gap-14 p-6">
       <h1 className="text-default text-xl font-extrabold">투표대기창</h1>
-      <VotingStatusCard />
+      {channelInfo && <VotingStatusCard info={channelInfo} />}
     </div>
   );
 }
 
-export { WaitingRoomHeader };
+export { WaitingRoomHeader, type WaitingRoomInfo };
