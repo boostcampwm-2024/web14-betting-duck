2f1cb77a97be4ac446ac03baad9dd798ecfacbd0
정동교 <jdk8467@naver.com>
2024-11-24 15:30:32 +0900

    feat: 베팅 시작 시 시작 이벤트 발행 [#176]

--- backend/src/bet-room/bet-room.service.ts
+++ backend/src/bet-room/bet-room.service.ts
@@ -83,6 +83,10 @@ export class BetRoomService {
     });
 
     await this.redisManager.setRoomStatus(betRoomId, "active");
+    this.betGateway.server.to(betRoomId).emit("startBetting", {
+      message: "베팅이 시작되었습니다.",
+      roomId: betRoomId,
+    });
     return updateResult;
   }
 
