ad8edfd9e29dd0d9791dba63fae73def7b12270d
sunub <bsc5672@gmail.com>
2024-11-25 12:53:13 +0900

    feat :프로젝트에서 사용하는 path에 대한 정보를 상수로 관리하게 끔 작성 [#54]

---
+++ frontend/src/shared/config/route.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const ROUTE_PATHS = {
+  LOGIN: "/login",
+  BETTING: "/betting",
+  BETTING_WAITING: "/betting_/$roomId/waiting",
+  BETTING_VOTE: "/betting_/$roomId/vote",
+  CREATE_VOTE: "/create-vote",
+  MYPAGE: "/my-page",
+  REQUIRE_LOGIN: "/require-login",
+} as const;
+
+type RouteKeys = keyof typeof ROUTE_PATHS;
+type Routes = {
+  readonly [K in RouteKeys]: string;
+};
+
+const ROUTES: Routes = Object.freeze(ROUTE_PATHS);
+
+// zod enum 스키마 생성
+const ROUTE_PATH_ENUM = z.enum([
+  "/login",
+  "/betting",
+  "/betting_/$roomId/waiting",
+  "/betting_/$roomId/vote",
+  "/create-vote",
+  "/my-page",
+  "/require-login",
+]);
+
+const ROUTES_PATH = ROUTE_PATH_ENUM.options;
+
+export { ROUTES, ROUTES_PATH, ROUTE_PATH_ENUM, type Routes };
