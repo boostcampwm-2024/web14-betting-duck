1caa9dbf5b4bfa83628fbf99f6ca2fc6ad70ceae
sunub <bsc5672@gmail.com>
2024-11-25 12:54:34 +0900

    feat : 소켓에 대한 연결이 페이지를 벗어날 경우 자동으로 연결이 끊기고 다시 페이지에 들어올 경우 소켓이 연결되게 끔 로직 작성 [#54]

--- frontend/src/shared/hooks/use-effect-once.tsx
+++ frontend/src/shared/hooks/use-effect-once.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 
-function useEffectOnce(callback: () => void) {
-  const isUsedRef = React.useRef(false);
-  const callbackRef = React.useRef(callback);
+function useEffectOnce(effect: () => (() => void) | void) {
+  const destroyRef = React.useRef<(() => void) | void>();
+  const effectCalled = React.useRef(false);
+  const renderAfterCalled = React.useRef(false);
+  const [, setVal] = React.useState<number>(0);
+
+  if (effectCalled.current) {
+    renderAfterCalled.current = true;
+  }
 
   React.useEffect(() => {
-    if (!isUsedRef.current) {
-      isUsedRef.current = true;
-      callbackRef.current();
+    // 이미 effect가 호출되었다면 더 이상 실행하지 않음
+    if (!effectCalled.current) {
+      destroyRef.current = effect();
+      effectCalled.current = true;
+      setVal((val) => val + 1); // 리렌더링 강제
     }
-  }, []);
+
+    return () => {
+      // cleanup은 항상 실행되어야 함
+      if (destroyRef.current) {
+        destroyRef.current();
+      }
+    };
+  }, [effect]);
+
+  return effectCalled.current;
 }
 
 export { useEffectOnce };
