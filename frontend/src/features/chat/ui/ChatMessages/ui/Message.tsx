206da0d045541b1def0a601346a503d7045787d6
sunub <bsc5672@gmail.com>
2024-11-25 12:34:44 +0900

    feat : 실제 데이터를 통해서 방의 정보를 업데이트 할 수 있게 끔 로직 작성
    기존에는 데모 데이터를 통해서 방에 참여하고 있었던 것을 실제 생성된 방을 기준으로 작성 되게 끔 수정하고 참여한 사용자의 정보를 채팅 방에서 확인할 수 있게 끔 로직 작성
    [#54]

--- frontend/src/features/chat/ui/ChatMessages/ui/Message.tsx
+++ frontend/src/features/chat/ui/ChatMessages/ui/Message.tsx
@@ -1,55 +1,55 @@
 import React from "react";
 
 interface MessageProps {
-  username: string;
+  sender: {
+    nickname: string;
+  };
   message: string;
-  userColor: string;
-  userIcon: string;
+  color: string;
+  radius: string;
 }
 
-const Message: React.FC<MessageProps> = ({
-  username,
-  message,
-  userColor,
-  userIcon,
-}) => {
-  return (
-    <div className="live_chatting_list_item__0SGhw">
-      <div className="live_chatting_message_container__vrI-y">
-        <span
-          className="live_chatting_username_container__m1-i5 live_chatting_username_is_message__jvTvP"
-          style={{ marginRight: "4px" }}
+const Message: React.FC<MessageProps> = (
+  { sender, message, color, radius }: MessageProps,
+  index: number,
+) => {
+  const firstMessageRegexp = /^(.+)님이 입장하셨습니다\.$/;
+  const firstMessage = firstMessageRegexp.exec(message);
+  if (firstMessage) {
+    return (
+      <div key={sender.nickname + index} className="flex justify-center">
+        <div
+          className={`max-w-[100%] ${radius} flex flex-row items-start gap-4 py-1`}
         >
-          <span className="live_chatting_username_wrapper__iJpJB">
-            <span className="live_chatting_username_icon__6Dj7b">
-              <span className="badge_container__a64XB">
-                <img src={userIcon} alt="" width="18" height="18" />
-              </span>
+          <div className="w-full">
+            <span
+              className={`${color} text-md mr-3 whitespace-nowrap font-bold`}
+            >
+              {firstMessage[1]}
             </span>
-          </span>
-          <span
-            className="live_chatting_username_nickname__dDbbj"
-            style={{ color: userColor }}
-          >
-            <span className="">
-              <span className="name_text__yQG50">{username}</span>
+            <span className="text-default break-words">
+              님이 입장하셨습니다.
             </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div key={sender.nickname + index} className="flex justify-end">
+      <div
+        className={`max-w-[80%] ${radius} flex flex-row items-start gap-4 bg-white p-3 shadow-sm`}
+      >
+        <div className="max-w-[300px]">
+          <span className={`${color} text-md mr-3 whitespace-nowrap font-bold`}>
+            {sender.nickname}
           </span>
-        </span>
-        <span className="live_chatting_message_text__DyleH">{message}</span>
+          <span className="break-words text-gray-700">{message}</span>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Message;
-
-// display: flex;
-// flex: 1 1;
-// margin: -77px 0 -62px;
-// overflow: hidden;
-// padding: 77px 0 62px;
-// position: relative;
-
-// margin-top: -77px;
-// padding-top: 77px;
