9f9fe10036b8286308671177a58e3b8fe48fef0f
sunub <bsc5672@gmail.com>
2024-11-25 12:47:57 +0900

    feat : 관리자가 방을 취소하고 방을 시작할 수 있게 끔 기능 추가 [#54]

--- frontend/src/features/waiting-room/ui/AdminFooter/CancleButton.tsx
+++ frontend/src/features/waiting-room/ui/AdminFooter/CancleButton.tsx
@@ -1,6 +1,31 @@
+import { useNavigate } from "@tanstack/react-router";
+import { useWaitingContext } from "../../hooks/use-waiting-context";
+import { useUserContext } from "@/shared/hooks/use-user-context";
+
 function CancleVottingButton() {
+  const navigate = useNavigate();
+  const { roomId } = useWaitingContext();
+  const { setUserInfo } = useUserContext();
+
+  async function cancleBettingRoom() {
+    try {
+      const response = await fetch(`/api/betrooms/${roomId}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) throw new Error("방 삭제에 실패했습니다.");
+
+      setUserInfo({ role: "user", roomId: undefined });
+      navigate({ to: "/my-page" });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <button className="bg-default text-secondary w-full rounded-lg p-[10px]">
+    <button
+      onClick={cancleBettingRoom}
+      className="bg-default text-secondary w-full rounded-lg p-[10px]"
+    >
       투표 취소
     </button>
   );
--- frontend/src/features/waiting-room/ui/AdminFooter/StartVotingButton.tsx
+++ frontend/src/features/waiting-room/ui/AdminFooter/StartVotingButton.tsx
@@ -1,6 +1,28 @@
+import { useNavigate } from "@tanstack/react-router";
+import { useWaitingContext } from "../../hooks/use-waiting-context";
+
 function StartVotingButton() {
+  const navigate = useNavigate();
+  const { roomId, setIsBettingStarted } = useWaitingContext();
+
+  async function startBettingRoom() {
+    try {
+      const response = await fetch(`/api/betrooms/start/${roomId}`, {
+        method: "PATCH",
+      });
+      if (!response.ok) throw new Error("배팅 시작에 실패했습니다.");
+      setIsBettingStarted(true);
+      navigate({ to: "../vote" });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <button className="bg-default text-secondary w-full rounded-lg p-[10px]">
+    <button
+      onClick={startBettingRoom}
+      className="bg-default text-secondary w-full rounded-lg p-[10px]"
+    >
       투표 시작
     </button>
   );
