78c9335bed38e3b8b52b9d4961edda4bcfb60306
sunub <bsc5672@gmail.com>
2024-11-25 12:28:16 +0900

    feat : 기존에 에러 코드를 통해서 접근 권한을 관리하던 방법을 수정
    기존에는 접근 권한에 문제가 생겼을 경우 모든 상태를 에러로 처리하다 보니 콘솔이 더러워지고 실제 에러가 처리하는 작업과 구분이 되지 않았던 문제가 있어서 이 부분을 개선하여 에러 처리를 하는 페이지로 리다이렉트 되게 끔 수정하였다.
    [#54]

--- frontend/src/routes/my-page.tsx
+++ frontend/src/routes/my-page.tsx
@@ -1,16 +1,20 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { ErrorComponent } from "@/shared/ui/error";
 import { MyPage } from "@/features/my-page";
 import { ErrorMyPage } from "@/features/my-page/error";
+import { ROUTES } from "@/shared/config/route";
 
 export const Route = createFileRoute("/my-page")({
-  component: MyPage,
-  loader: async () => {
+  beforeLoad: async () => {
     const response = await fetch("/api/users/token");
     if (!response.ok) {
-      throw new Error("로그인이 필요합니다.");
+      throw redirect({
+        to: "/require-login",
+        search: { from: encodeURIComponent(ROUTES.MYPAGE) },
+      });
     }
   },
+  component: MyPage,
   errorComponent: ({ error }) => (
     <ErrorComponent error={error} feature="마이 페이지" to="/demo-login">
       <ErrorMyPage />
