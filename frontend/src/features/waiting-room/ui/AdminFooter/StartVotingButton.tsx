966a4b614ac24fcff6530f965a774f09b5b9f8e4
죠 <stop.ho@khu.ac.kr>
2024-11-29 00:33:45 +0900

    feat: wating page에서 관리자가 투표 시작을 누르면 바로 관리자 페이지로 이동되도록 설정 [#60]

--- frontend/src/features/waiting-room/ui/AdminFooter/StartVotingButton.tsx
+++ frontend/src/features/waiting-room/ui/AdminFooter/StartVotingButton.tsx
@@ -12,7 +12,7 @@ function StartVotingButton() {
       });
       if (!response.ok) throw new Error("배팅 시작에 실패했습니다.");
       setIsBettingStarted(true);
-      navigate({ to: "../vote/voting" });
+      navigate({ to: "../vote/admin" });
     } catch (error) {
       console.error(error);
     }
