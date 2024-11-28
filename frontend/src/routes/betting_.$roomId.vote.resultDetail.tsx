79b4900cb1aade544e16bc05a9d747ffacd409c6
죠 <stop.ho@khu.ac.kr>
2024-11-29 03:27:45 +0900

    feat: role에 따른 footer 렌더링 기능 구현 [#59]

--- frontend/src/routes/betting_.$roomId.vote.resultDetail.tsx
+++ frontend/src/routes/betting_.$roomId.vote.resultDetail.tsx
@@ -1,10 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { PredictDetail } from "@/features/predict-detail";
+import { UserProvider } from "@/app/provider/UserProvider";
 
 export const Route = createFileRoute("/betting_/$roomId/vote/resultDetail")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <PredictDetail />;
+  return (
+    <UserProvider>
+      <PredictDetail />
+    </UserProvider>
+  );
 }
