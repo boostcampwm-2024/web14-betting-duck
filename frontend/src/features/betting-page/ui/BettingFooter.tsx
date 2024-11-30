2e89e4c476c1c5bb9965d22781f739c12dc21b15
sunub <bsc5672@gmail.com>
2024-12-01 03:49:00 +0900

    feat : 투표 시 계산되는 사용자 금액 로직 수정

--- frontend/src/features/betting-page/ui/BettingFooter.tsx
+++ frontend/src/features/betting-page/ui/BettingFooter.tsx
@@ -1,20 +1,12 @@
-import { useUserContext } from "@/shared/hooks/useUserContext";
 import { DuckCoinIcon } from "@/shared/icons";
-import { Route } from "@/routes/betting_.$roomId.vote";
-import React from "react";
+import { Route } from "@/routes/betting_.$roomId.vote.voting";
+import { useBettingContext } from "../hook/useBettingContext";
 
 function BettingFooter() {
-  const { userInfo } = useUserContext();
-  const { duckCoin } = Route.useLoaderData();
-  const [currentDuckCoin, setCurrentDuckCoin] = React.useState(duckCoin);
-
-  const { isPlaceBet, placeBetAmount } = userInfo;
-
-  React.useEffect(() => {
-    if (isPlaceBet) {
-      setCurrentDuckCoin(duckCoin - (placeBetAmount ?? 0));
-    }
-  }, [placeBetAmount, duckCoin, isPlaceBet]);
+  const { bettingPool } = useBettingContext();
+  const { userInfo } = Route.useLoaderData();
+  const { duck } = userInfo;
+  const { placeBetAmount } = bettingPool;
 
   return (
     <div className="flex items-center justify-between gap-2 px-4 pt-4 text-center font-bold text-gray-600">
@@ -29,7 +21,7 @@ function BettingFooter() {
         소유 금액:{" "}
         <span className="flex items-center gap-2 font-extrabold">
           <DuckCoinIcon width={26} height={26} />
-          {currentDuckCoin}
+          {duck - (placeBetAmount ?? 0)}
         </span>
       </div>
     </div>
