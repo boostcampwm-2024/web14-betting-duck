eed42f0c32f6f7dc182f1edfce7f9eeca05bd19d
sunub <bsc5672@gmail.com>
2024-11-30 17:21:56 +0900

    feat : 라우터에 tanstack query 추가 [#61]

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,29 +2,37 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen.ts";
-import { Auth } from "@/shared/lib/auth.ts";
+import { routeTree } from "./routeTree.gen";
+import { Auth } from "@/shared/lib/auth/auth";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { GlobalErrorComponent } from "./shared/components/Error/GlobalError.tsx";
+import { GlobalErrorComponent } from "./shared/components/Error/GlobalError";
+import { QueryClient } from "@tanstack/react-query";
+import { queryClient } from "@/shared/lib/auth/authQuery";
+
+type RouterContext = {
+  auth: Auth;
+  queryClient: QueryClient;
+};
 
 const router = createRouter({
   routeTree,
   context: {
-    auth: Auth,
-  },
+    auth: {} as Auth,
+    queryClient,
+  } as RouterContext,
   defaultPreload: "intent",
   defaultErrorComponent: ({ error, reset }) => (
     <GlobalErrorComponent error={error} reset={reset} />
   ),
 });
 
-const theme = createTheme();
-
 declare module "@tanstack/react-router" {
-  interface RouterState {
+  interface Register {
     router: typeof router;
   }
 }
+
+const theme = createTheme();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -33,3 +41,5 @@ createRoot(document.getElementById("root")!).render(
     </ThemeProvider>
   </StrictMode>,
 );
+
+export { type RouterContext };
