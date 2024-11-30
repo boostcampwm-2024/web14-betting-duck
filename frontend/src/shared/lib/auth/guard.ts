4cbff8fc9a76bdbc3dc63686f121395a96eed5dc
sunub <bsc5672@gmail.com>
2024-11-30 16:52:25 +0900

    feat : tanstack query를 사용하여 자주 사용되는 사용자 검증 요청을 캐싱 [#61]

---
+++ frontend/src/shared/lib/auth/guard.ts
@@ -0,0 +1,118 @@
+import { responseUserInfoSchema } from "@betting-duck/shared";
+import { redirect } from "@tanstack/react-router";
+import { z } from "zod";
+
+export async function requireAuth({ to }: { to: string }) {
+  try {
+    const [tokenResponse, userInfoResponse] = await Promise.all([
+      fetch("/api/users/token", { credentials: "include" }),
+      fetch("/api/users/userInfo", { credentials: "include" }),
+    ]);
+
+    if (!tokenResponse.ok) {
+      throw redirect({
+        to: "/require-login",
+        search: { from: encodeURIComponent(to) },
+      });
+    }
+
+    if (!userInfoResponse.ok) {
+      throw new Error("사용자 정보를 불러오는데 실패했습니다.");
+    }
+
+    const { data } = await userInfoResponse.json();
+    const userInfo = responseUserInfoSchema.safeParse(data);
+
+    if (!userInfo.success) {
+      throw new Error("사용자 정보를 파싱하는데 실패했습니다.");
+    }
+
+    return userInfo.data;
+  } catch (error) {
+    console.error(error);
+    throw redirect({
+      to: "/require-login",
+      search: { from: encodeURIComponent(to) },
+    });
+  }
+}
+
+export async function requireUesrRole({
+  userInfo,
+  to,
+}: {
+  userInfo: z.infer<typeof responseUserInfoSchema>;
+  to: string;
+}) {
+  if (userInfo.role === "guest") {
+    throw redirect({
+      to: "/require-login",
+      search: { from: encodeURIComponent(to) },
+    });
+  }
+
+  return userInfo;
+}
+
+type UserInfoType = z.infer<typeof responseUserInfoSchema>;
+
+type AuthStatusType = {
+  isAuthenticated: boolean;
+  userInfo: UserInfoType;
+};
+
+const defaultUserInfo: UserInfoType = {
+  message: "OK",
+  role: "user",
+  nickname: "",
+  duck: 0,
+};
+
+export const AuthStatusTypeSchema = z.object({
+  isAuthenticated: z.boolean(),
+  userInfo: responseUserInfoSchema,
+});
+
+export async function checkAuthStatus(): Promise<AuthStatusType> {
+  try {
+    const [tokenResponse, userInfoResponse] = await Promise.all([
+      fetch("/api/users/token", {
+        headers: {
+          "Cache-Control": "stale-while-revalidate",
+          Pragma: "no-cache",
+        },
+        credentials: "include",
+      }),
+      fetch("/api/users/userInfo", {
+        credentials: "include",
+      }),
+    ]);
+
+    if (!tokenResponse.ok || !userInfoResponse.ok) {
+      return {
+        isAuthenticated: false,
+        userInfo: defaultUserInfo,
+      };
+    }
+
+    const { data } = await userInfoResponse.json();
+    const result = responseUserInfoSchema.safeParse(data);
+
+    if (!result.success) {
+      return {
+        isAuthenticated: false,
+        userInfo: defaultUserInfo,
+      };
+    }
+
+    return {
+      isAuthenticated: true,
+      userInfo: result.data,
+    };
+  } catch {
+    return {
+      isAuthenticated: false,
+      userInfo: defaultUserInfo,
+    };
+  }
+}
