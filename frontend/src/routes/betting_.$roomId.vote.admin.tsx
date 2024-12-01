f1ccdc589795446c1cad59b6c474b08f2532b404
sunub <bsc5672@gmail.com>
2024-12-02 02:34:40 +0900

    feat : loader 데이터를 이용하여 정적인 데이터를 사용할 수 있게끔 작성 [#61]

--- frontend/src/routes/betting_.$roomId.vote.admin.tsx
+++ frontend/src/routes/betting_.$roomId.vote.admin.tsx
@@ -1,6 +1,56 @@
 import { BettingPageAdmin } from "@/features/betting-page-admin";
-import { createFileRoute } from "@tanstack/react-router";
+import { getBettingRoomInfo } from "@/features/betting-page/api/getBettingRoomInfo";
+import { STORAGE_KEY } from "@/features/betting-page/model/var";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { loadBetRoomData } from "@/shared/lib/loader/useBetRoomLoader";
+import {
+  responseBetRoomInfo,
+  responseUserInfoSchema,
+} from "@betting-duck/shared";
+import { z } from "zod";
+import { queryClient } from "@/shared/lib/auth/authQuery";
+
+type BetRoomResponse = z.infer<typeof responseBetRoomInfo>;
+type UserInfo = z.infer<typeof responseUserInfoSchema>;
+interface RouteLoaderData {
+  roomId: string;
+  bettingRoomInfo: BetRoomResponse;
+  userInfo: UserInfo;
+}
 
 export const Route = createFileRoute("/betting_/$roomId/vote/admin")({
   component: BettingPageAdmin,
+  beforeLoad: async ({ params }) => {
+    const { roomId } = params;
+    const roomInfo = await getBettingRoomInfo(roomId);
+    if (!roomInfo) {
+      throw new Error("방 정보를 불러오는데 실패했습니다.");
+    }
+
+    if (!roomInfo.channel.isAdmin) {
+      throw redirect({
+        to: `/betting/${roomId}/vote/voting`,
+      });
+    }
+  },
+  loader: async ({ params, abortController }): Promise<RouteLoaderData> => {
+    const { roomId } = params;
+
+    const { bettingRoomInfo, userInfo } = await loadBetRoomData({
+      roomId,
+      signal: abortController.signal,
+      queryClient,
+    });
+
+    return { roomId, bettingRoomInfo, userInfo };
+  },
+  onLeave: async ({ params }) => {
+    const { roomId } = params;
+    const bettingRoomInfo = await getBettingRoomInfo(roomId);
+
+    if (bettingRoomInfo?.channel.status !== "active") {
+      console.log(bettingRoomInfo);
+      return sessionStorage.removeItem(STORAGE_KEY);
+    }
+  },
 });
--- frontend/src/routes/betting_.$roomId.vote.resultDetail.tsx
+++ frontend/src/routes/betting_.$roomId.vote.resultDetail.tsx
@@ -1,15 +1,13 @@
-import { createFileRoute } from "@tanstack/react-router";
 import { PredictDetail } from "@/features/predict-detail";
-import { UserProvider } from "@/app/provider/UserProvider";
+import { getBetResults } from "@/features/predict-detail/model/api";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/betting_/$roomId/vote/resultDetail")({
-  component: RouteComponent,
-});
+  component: PredictDetail,
+  loader: async ({ params }) => {
+    const { roomId } = params;
 
-function RouteComponent() {
-  return (
-    <UserProvider>
-      <PredictDetail />
-    </UserProvider>
-  );
-}
+    const betResults = await getBetResults(roomId);
+    return betResults;
+  },
+});
