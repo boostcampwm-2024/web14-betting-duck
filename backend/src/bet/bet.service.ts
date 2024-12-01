1670e4eccf30a69b9f77e3939ad74fbf4c9f4faf
정동교 <jdk8467@naver.com>
2024-12-02 01:29:36 +0900

    refactor: 베팅 시작 시 redis 유저 정보 초기화에 betAmount, selectedoption 추가 [#223]

--- backend/src/bet/bet.service.ts
+++ backend/src/bet/bet.service.ts
@@ -147,4 +147,30 @@ export class BetService {
     }
     throw new NotFoundException("해당 유저를 찾을 수 없습니다.");
   }
+
+  async getBetDetail(req: Request, betRoomId: string) {
+    const userId = req["user"].id;
+    const userRole = req["user"].role;
+    const cachedBetDetails = await this.redisManager.getBettingUser(
+      String(userId),
+      betRoomId,
+    );
+
+    if (cachedBetDetails.nickname) {
+      const { betAmount, selectedOption } = cachedBetDetails;
+      return { betAmount: Number(betAmount), selectedOption };
+    }
+
+    if (userRole === "user") {
+      const betDetails = await this.betRepository.findByUserAndRoom(
+        userId,
+        betRoomId,
+      );
+      if (betDetails) {
+        const { betAmount, selectedOption } = betDetails;
+        return { betAmount, selectedOption };
+      }
+    }
+    throw new NotFoundException("해당 유저 베팅 내역을 찾을 수 없습니다.");
+  }
 }
