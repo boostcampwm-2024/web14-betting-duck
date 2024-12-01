1b0b1b96ed0ed1a9e267661153db1528eae875d8
죠 <stop.ho@khu.ac.kr>
2024-12-01 21:52:00 +0900

    feat: admin Router에 loader 추가 [#59]

--- frontend/src/routes/betting_.$roomId.vote.admin.tsx
+++ frontend/src/routes/betting_.$roomId.vote.admin.tsx
@@ -1,6 +1,44 @@
 import { BettingPageAdmin } from "@/features/betting-page-admin";
 import { createFileRoute } from "@tanstack/react-router";
+import { loadBetRoomData } from "@/shared/lib/loader/useBetRoomLoader";
+import { queryClient } from "@/shared/lib/auth/authQuery";
+import {
+  responseBetRoomInfo,
+  responseUserInfoSchema,
+} from "@betting-duck/shared";
+import { z } from "zod";
+import { getBettingRoomInfo } from "@/features/betting-page/api/getBettingRoomInfo";
+import { STORAGE_KEY } from "@/features/betting-page/model/var";
+
+type BetRoomResponse = z.infer<typeof responseBetRoomInfo>;
+type UserInfo = z.infer<typeof responseUserInfoSchema>;
+
+interface RouteLoaderData {
+  roomId: string;
+  bettingRoomInfo: BetRoomResponse;
+  userInfo: UserInfo;
+}
 
 export const Route = createFileRoute("/betting_/$roomId/vote/admin")({
   component: BettingPageAdmin,
+  loader: async ({ params, abortController }): Promise<RouteLoaderData> => {
+    const { roomId } = params;
+
+    const { bettingRoomInfo, userInfo } = await loadBetRoomData({
+      roomId,
+      signal: abortController.signal,
+      queryClient,
+    });
+
+    return { roomId, bettingRoomInfo, userInfo };
+  },
+  onLeave: async ({ params }) => {
+    const { roomId } = params;
+    const bettingRoomInfo = await getBettingRoomInfo(roomId);
+
+    if (bettingRoomInfo?.channel.status !== "active") {
+      console.log(bettingRoomInfo);
+      return sessionStorage.removeItem(STORAGE_KEY);
+    }
+  },
 });
