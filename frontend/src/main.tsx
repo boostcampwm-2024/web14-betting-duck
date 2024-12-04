f864f3870ab5cd178016300f8755144745d33685
sunub <bsc5672@gmail.com>
2024-12-05 03:52:42 +0900

    feat : tanstack query 추가 [#61]

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,15 +6,27 @@ import { routeTree } from "./routeTree.gen";
 import { Auth } from "@/shared/lib/auth/auth";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { GlobalErrorComponent } from "./shared/components/Error/GlobalError";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 type RouterContext = {
   auth: Auth;
+  queryClient: QueryClient;
 };
 
 const router = createRouter({
   routeTree,
   context: {
     auth: {} as Auth,
+    queryClient,
   } as RouterContext,
   defaultPreload: "intent",
   defaultErrorComponent: ({ error }) => (
@@ -32,9 +44,11 @@ const theme = createTheme();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
 
