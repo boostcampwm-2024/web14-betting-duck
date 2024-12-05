26f9b41e8680a5c9a58f7aaf9abc92d55aff25dd
sunub <bsc5672@gmail.com>
2024-12-05 17:42:56 +0900

    feat : 토큰이 존재할 경우 my-page로 이동할 수 있게 로직 추가

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,9 +1,21 @@
 import { LoginPage } from "@/features/login-page";
 import { GlobalErrorComponent } from "@/shared/components/Error/GlobalError";
-import { createFileRoute } from "@tanstack/react-router";
+import { authQueries } from "@/shared/lib/auth/authQuery";
+import { AuthStatusTypeSchema } from "@/shared/lib/auth/guard";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
   component: Component,
+  loader: async ({ context: { queryClient } }) => {
+    const queryData = await queryClient.ensureQueryData(authQueries);
+    const parsedData = AuthStatusTypeSchema.safeParse(queryData);
+
+    if (parsedData.success && parsedData.data.isAuthenticated) {
+      return redirect({
+        to: "/my-page",
+      });
+    }
+  },
   errorComponent: ({ error }) => <GlobalErrorComponent error={error} to="/" />,
 });
 
