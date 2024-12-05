4654990876c93af0a17ef6d6fe509476e47aca3a
sunub <bsc5672@gmail.com>
2024-12-05 14:05:40 +0900

    fix : 라우트에서 결과를 제대로 처리하지 못했던 문제 수정 [#61]

--- frontend/src/routes/betting_.$roomId.vote.resultDetail.tsx
+++ frontend/src/routes/betting_.$roomId.vote.resultDetail.tsx
@@ -1,5 +1,8 @@
+import { STORAGE_KEY } from "@/features/betting-page/model/var";
 import { PredictDetail } from "@/features/predict-detail";
 import { GlobalErrorComponent } from "@/shared/components/Error/GlobalError";
+import { getSessionItem } from "@/shared/hooks/useSessionStorage";
+import { BettingRoomInfoSchema } from "@/shared/types";
 import { betResultResponseSchema } from "@betting-duck/shared";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { z } from "zod";
@@ -23,39 +26,66 @@ export const Route = createFileRoute("/betting_/$roomId/vote/resultDetail")({
     if (!bettingResultResponse.ok) {
       throw new Error("배팅 결과를 가져오는데 실패했습니다.");
     }
+
     const { data } = await bettingResultResponse.json();
     const bettingResult = betResultResponseSchema.safeParse(data);
-    if (!bettingResult.success) {
-      throw redirect({
-        to: "/my-page",
-        replace: true,
-      });
-    }
 
-    const personalBetResultResponse = await fetch(`/api/bets/${roomId}`);
-    if (!personalBetResultResponse.ok) {
-      throw redirect({
-        to: "/my-page",
-        replace: true,
-      });
-    }
-    const personalBetResult = await personalBetResultResponse.json();
-    const parsedPersonalBetResult = PersonalBetResultResponseSchema.safeParse(
-      personalBetResult.data,
-    );
-    if (!parsedPersonalBetResult.success) {
-      throw redirect({
-        to: "/my-page",
-        replace: true,
-      });
-    }
+    try {
+      const personalBetResultResponse = await fetch(`/api/bets/${roomId}`);
+      const personalBetResult = await personalBetResultResponse.json();
+      const parsedPersonalBetResult = PersonalBetResultResponseSchema.safeParse(
+        personalBetResult.data,
+      );
+      if (!parsedPersonalBetResult.success) {
+        throw redirect({
+          to: "/my-page",
+          replace: true,
+        });
+      }
 
-    return {
-      betResults: bettingResult.data,
-      personalBetResult: parsedPersonalBetResult.data,
-    };
+      return {
+        betResults: bettingResult.data,
+        personalBetResult: parsedPersonalBetResult.data,
+      };
+    } catch {
+      const session = await getSessionItem(STORAGE_KEY);
+      const personalBetResult = JSON.parse(session);
+      if (
+        !personalBetResult ||
+        !personalBetResult.placeBetAmount ||
+        !personalBetResult.selectedOption
+      ) {
+        throw redirect({
+          to: "/my-page",
+          replace: true,
+        });
+      }
+
+      const parsedPersonalBetResult = PersonalBetResultResponseSchema.safeParse(
+        {
+          betAmount: personalBetResult.placeBetAmount,
+          selectedOption: personalBetResult.selectedOption,
+        },
+      );
+      return {
+        betResults: bettingResult.data,
+        personalBetResult: parsedPersonalBetResult.data,
+      };
+    }
   },
   errorComponent: ({ error }) => {
     return <GlobalErrorComponent error={error} to="/" />;
+  },
+  onLeave: async ({ params, context: { queryClient } }) => {
+    const { roomId } = params;
+    const bettingData = queryClient.getQueryData(["bettingRoom", roomId]);
+    const bettingRoomInfo = BettingRoomInfoSchema.safeParse(bettingData);
+    if (!bettingRoomInfo.success) {
+      throw new Error("방 정보를 불러오는데 실패했습니다.");
+    }
+
+    if (bettingRoomInfo.data.channel.status !== "active") {
+      return sessionStorage.removeItem(STORAGE_KEY);
+    }
   },
 });
